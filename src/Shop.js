a92cc693e7187501884c4ad68784c3442478be82
Pranay-Rokade <pranayrokade86@gmail.com>
2025-03-14 22:39:05 +0530

    Completed the setup of the Redux App

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Puma Shirt Rs 50</h2>
+      <button className="btn btn-success mx-2">+</button>
+      Add to cart
+      <button className="btn btn-success mx-2">-</button>
+    </div>
+  )
+}
+
+export default Shop
